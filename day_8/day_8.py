53a612056ee63411c60ed87929b49ddd3bfb0f4b
kpham12-hash <kevinxpham.dev@gmail.com>
2026-07-31 12:22:44 -0400

    Complete Day 8 dictionary exercises

---
+++ day_8/day_8.py
@@ -0,0 +1,36 @@
+# Exercises: Day 8
+
+#1. Create an empty dictionary called dog
+dog = {}
+
+#2. Add name, color, breed, legs, age to the dog dictionary
+
+dog = {
+    'Name': 'Gerry',
+    'Breed': 'Chihushua',
+    'Legs': 'Paw',
+    'Age': '2' }
+
+print(dog)
+
+#3. Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary.
+
+student = {
+    'first_name': 'Kevin',
+    'last_name': 'Pham',
+    'gender': 'Male',
+    'age': '19',
+    'martial_Status': 'Single',
+    'Skills': ['Organization', 'Coding', 'Pay-attention To Detail', 'Teamwork', 'Communcation Skills', 'Event setup and Support', 'Reliablity'],
+    'Country': 'United States',
+    'City': 'Havertown',
+    'Address': '1519 Melrose Avenue, Havertown, PA, USA'
+}
+
+#4. Get the length of the student dictionary.
+
+print(len(student))
+
+#5. Get the value of skills and check the data type, it should be a list.
+
+#6. 
